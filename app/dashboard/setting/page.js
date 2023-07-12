21a153c9283d1f43b09c0c1a249b5e7b38628846
Lerian Febriana <kanglerian@MJLQ2s-MBP.lan>
2023-07-12 11:29:32 +0700

    first

---
+++ app/dashboard/setting/page.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../../components/Navbar'
+
+const Setting = () => {
+  return (
+    <div>
+      <Navbar />
+      <h1>Setting</h1>
+    </div>
+  )
+}
+
+export default Setting
