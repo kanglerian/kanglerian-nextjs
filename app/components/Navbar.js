be7926f2c253b95af8ca9fa157f5f6cd1fdcbc20
Lerian Febriana <kanglerian@MJLQ2s-MBP.lan>
2023-07-12 11:37:13 +0700

    navbar

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import Link from 'next/link'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div>
+      <ul>
+        <li><Link href='/'>Home</Link></li>
+        <li><Link href='/dashboard'>Dashboard</Link></li>
+        <li><Link href='/dashboard/setting'>Setting</Link></li>
+      </ul>
+    </div>
   )
 }
 
